01c4b49acd5116139ed1d799d4823df3191f12e6
Sara Sundin <sarasundin@rocketmail.com>
2025-04-09 21:48:54 +0200

    add home app and index.html template

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'products',
     'sizemeapp',
     'orders',
+    'home',
 
     # Django defaults
     'django.contrib.admin',
@@ -121,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # or os.path.join(BASE_DIR, "static") for older versions
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+    Display the Home page.
+    """
+    return render(request, "home/index.html")
+
