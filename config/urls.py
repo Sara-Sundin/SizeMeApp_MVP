87ec5a500ee262214c4d74187a6d0ce1c9057460
Sara Sundin <sarasundin@rocketmail.com>
2025-04-12 16:16:47 +0200

    serve images for shop from media in development

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')), 
     path('webshop/', include('webshop.urls')),
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
