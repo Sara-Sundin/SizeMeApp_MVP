1e9b1fb9a394f062c9c703c66bc9a787d0e5ef76
Sara Sundin <sarasundin@rocketmail.com>
2025-04-14 22:16:49 +0200

    add product and category admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')  # ← removed 'rating'
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
