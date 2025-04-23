eae4a49fb6ec025e22c7d51c4f17c7b0c4818313
Sara Sundin <sarasundin@rocketmail.com>
2025-04-23 21:21:37 +0200

    add modal for updating measurements in product detail view

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from webshop import views as sizemeapp_views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('update-measurements/', sizemeapp_views.update_measurements_from_webshop, name='update_measurements_from_webshop'),
 ]
--- webshop/views.py
+++ webshop/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib import messages
 
 def webshop_view(request):
     if 'size_mode' not in request.session:
         request.session['size_mode'] = True
     return render(request, 'webshop/webshop.html')
+
+
+@login_required
+def update_measurements_from_webshop(request):
+    if request.method == 'POST':
+        user = request.user
+        user.chest = request.POST.get('chest')
+        user.waist = request.POST.get('waist')
+        user.hips = request.POST.get('hips')
+        user.shoulders = request.POST.get('shoulders')
+        user.save()
+        messages.success(request, "Measurements updated.")
+    return redirect(request.META.get('HTTP_REFERER', 'products'))
+
