f71affcf8aa61011a8026f49e6861bdb8548daa0
Sara Sundin <sarasundin@rocketmail.com>
2025-05-09 11:20:38 +0200

    Clean up urls in account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 from .views import (
     update_measurements,
@@ -7,13 +9,12 @@ from .views import (
     SignupView,
     CustomLoginView,
     ProfileUpdateView,
+    update_avatar,
 )
-from django.contrib.auth import views as auth_views
-from .views import update_avatar
-
 
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
+
     path(
         'login-update-measurements/',
         views.login_update_measurements,
@@ -29,6 +30,7 @@ urlpatterns = [
         views.delete_measurements,
         name='delete_measurements'
     ),
+
     path('logout/', custom_logout, name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -36,25 +38,38 @@ urlpatterns = [
         'update-profile/',
         ProfileUpdateView.as_view(),
         name='update_profile'
-        ),
+    ),
     path('delete-account/', delete_account, name='delete_account'),
-        path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='accounts/password_reset.html'
-    ), name='password_reset'),
 
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='accounts/password_reset_done.html'
-    ), name='password_reset_done'),
-
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='accounts/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
-
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='accounts/password_reset_complete.html'
-    ), name='password_reset_complete'),
+    # Password reset
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 
     path('update-avatar/', update_avatar, name='update_avatar'),
 ]
-
-
